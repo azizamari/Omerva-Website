9a64f428ea3f1a60ca24c8662412bfbd97111581
azizamari <azizamari2003@gmail.com>
2021-08-24 18:07:32 +0100

    Fix layout and remove sign button and easter egg

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
+const links=document.querySelectorAll('.side_links')
 function openSlideMenu(){
     document.getElementById('side-menu').style.width ='200px';
+    for(const link of links){
+        link.style.display ='block';
+    }
 }
 
 function closeSlideMenu(){
+    for(const link of links){
+        link.style.display ='none';
+    }
     document.getElementById('side-menu').style.width ='0px';
 }
 
-const links=document.querySelectorAll('.side_links')
 for(const link of links){
     link.addEventListener('click',()=>{
         closeSlideMenu();
