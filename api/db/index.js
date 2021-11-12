e8a5a8753ec92534317609cc07370a83eb0aff7d
azizamari <azizamari2003@gmail.com>
2021-11-12 13:27:33 +0100

    make website static

--- api/db/index.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize}=require('sequelize')
-
-const sequelize = new Sequelize('immerva-blog','postgres',process.env.DB_PASSWORD,{
-    dialect: 'postgres',
-    port:'6969'
-})
-
-module.exports = sequelize
